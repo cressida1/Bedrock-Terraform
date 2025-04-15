de1807617d807ef4dad52b9004948d802e07657f
cressida1 <51233417+cressida1@users.noreply.github.com>
2025-04-15 13:59:19 +0530

    Initial terraform code

--- test/main.tf
+++ test/main.tf
@@ -1,34 +1,8 @@
-Based on the architecture diagram and the provided configuration, here's the Terraform code to create the infrastructure:
+Based on the provided architecture diagram and the specified configuration, here's the Terraform code to create the infrastructure:
 
 ```hcl
 provider "aws" {
   region = "us-east-1"
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  
-  tags = {
-    Name = "main-vpc"
-  }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  
-  tags = {
-    Name = "Public Subnet"
-  }
-}
-
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  
-  tags = {
-    Name = "Private Subnet"
-  }
 }
 
 resource "aws_elastic_beanstalk_application" "migration_ui" {
@@ -39,7 +13,7 @@ resource "aws_elastic_beanstalk_application" "migration_ui" {
 resource "aws_elastic_beanstalk_environment" "migration_ui" {
   name                = "migration-ui-env"
   application         = aws_elastic_beanstalk_application.migration_ui.name
-  solution_stack_name = "64bit Amazon Linux 2 v5.8.0 running Node.js 14"
+  solution_stack_name = "64bit Amazon Linux 2 v5.8.0 running Node.js 16"
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
@@ -60,21 +34,15 @@ resource "aws_elastic_beanstalk_environment" "migration_ui" {
   }
 
   setting {
-    namespace = "aws:ec2:vpc"
-    name      = "VPCId"
-    value     = aws_vpc.main.id
-  }
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "Subnets"
-    value     = aws_subnet.public.id
-  }
-
-  setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "LoadBalancerType"
     value     = "application"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBScheme"
+    value     = "public"
   }
 }
 
@@ -86,7 +54,7 @@ resource "aws_elastic_beanstalk_application" "migration_api" {
 resource "aws_elastic_beanstalk_environment" "migration_api" {
   name                = "migration-api-env"
   application         = aws_elastic_beanstalk_application.migration_api.name
-  solution_stack_name = "64bit Amazon Linux 2 v5.8.0 running Node.js 14"
+  solution_stack_name = "64bit Amazon Linux 2 v5.8.0 running Node.js 16"
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
@@ -107,35 +75,43 @@ resource "aws_elastic_beanstalk_environment" "migration_api" {
   }
 
   setting {
-    namespace = "aws:ec2:vpc"
-    name      = "VPCId"
-    value     = aws_vpc.main.id
-  }
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "Subnets"
-    value     = aws_subnet.private.id
-  }
-
-  setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "LoadBalancerType"
     value     = "application"
   }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBScheme"
+    value     = "internal"
+  }
 }
 
-resource "aws_security_group" "migration_api" {
-  name        = "migration-api-sg"
-  description = "Security group for Migration API"
-  vpc_id      = aws_vpc.main.id
+resource "aws_security_group_rule" "allow_ui_to_api" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_elastic_beanstalk_environment.migration_ui.security_group_id
+  security_group_id        = aws_elastic_beanstalk_environment.migration_api.security_group_id
+}
 
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_elastic_beanstalk_environment.migration_ui.load_balancers[0]]
-  }
+resource "aws_lambda_function" "start_migration" {
+  filename      = "start_migration.zip"
+  function_name = "start-migration"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "python3.13"
+  timeout       = 900
+}
+
+resource "aws_lambda_function" "rollback_migration" {
+  filename      = "rollback_migration.zip"
+  function_name = "rollback-migration"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "python3.13"
+  timeout       = 900
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -160,22 +136,25 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_full_access" {
   role       = aws_iam_role.lambda_role.name
 }
 
-resource "aws_lambda_function" "start_migration" {
-  filename      = "start_migration.zip"
-  function_name = "start-migration"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
-  runtime       = "python3.13"
-  timeout       = 900
-}
+resource "aws_sfn_state_machine" "migration_state_machine" {
+  name     = "Migration"
+  role_arn = aws_iam_role.step_function_role.arn
 
-resource "aws_lambda_function" "rollback_migration" {
-  filename      = "rollback_migration.zip"
-  function_name = "rollback-migration"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
-  runtime       = "python3.13"
-  timeout       = 900
+  definition = jsonencode({
+    StartAt = "StartMigration"
+    States = {
+      StartMigration = {
+        Type     = "Task"
+        Resource = aws_lambda_function.start_migration.arn
+        Next     = "RollbackMigration"
+      }
+      RollbackMigration = {
+        Type     = "Task"
+        Resource = aws_lambda_function.rollback_migration.arn
+        End      = true
+      }
+    }
+  })
 }
 
 resource "aws_iam_role" "step_function_role" {
@@ -200,41 +179,20 @@ resource "aws_iam_role_policy_attachment" "step_function_lambda_full_access" {
   role       = aws_iam_role.step_function_role.name
 }
 
-resource "aws_sfn_state_machine" "migration" {
-  name     = "Migration"
-  role_arn = aws_iam_role.step_function_role.arn
-
-  definition = jsonencode({
-    StartAt = "StartMigration"
-    States = {
-      StartMigration = {
-        Type     = "Task"
-        Resource = aws_lambda_function.start_migration.arn
-        Next     = "RollbackMigration"
-      }
-      RollbackMigration = {
-        Type     = "Task"
-        Resource = aws_lambda_function.rollback_migration.arn
-        End      = true
-      }
-    }
-  })
-}
-
-resource "aws_route53_zone" "main" {
-  name = "test.com"
-}
-
 resource "aws_route53_record" "migration_ui" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "migration-ui.test.com"
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "test.com"
   type    = "A"
 
   alias {
     name                   = aws_elastic_beanstalk_environment.migration_ui.cname
-    zone_id                = aws_elastic_beanstalk_environment.migration_ui.load_balancers[0]
+    zone_id                = aws_elastic_beanstalk_environment.migration_ui.zone_id
     evaluate_target_health = true
   }
+}
+
+data "aws_route53_zone" "main" {
+  name = "test.com"
 }
 
 resource "aws_autoscaling_policy" "cpu_utilization" {
@@ -251,4 +209,4 @@ resource "aws_autoscaling_policy" "cpu_utilization" {
 }
 ```
 
-This Terraform code creates the infrastructure based on the provided architecture diagram and configuration. It includes the VPC, subnets, Elastic Beanstalk applications and environments, Lambda functions, Step Function, Route 53 records, and auto-scaling policies. Note that some aspects, like the specific AMI for EC2 instances, are managed by Elastic Beanstalk and not directly specified in this code.
+This Terraform code creates the infrastructure based on the provided architecture diagram and configuration. It includes Elastic Beanstalk applications for the UI and API, Lambda functions, Step Functions, Route 53 records, and auto-scaling policies. Note that some aspects, like VPC and subnet configuration, are using default settings as per the provided configuration.
